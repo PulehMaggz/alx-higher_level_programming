9a76b130e4ff498c8b4fc27521354347cc470c36
PulehMaggz <pulehmaggz@gmail.com>
2024-09-10 12:45:33 -0700

    12-fizzbuzz.py

---
+++ 0x01-python-if_else_loops_functions/12-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# Import the fizzbuzz function from 12-fizzbuzz.py
+fizzbuzz = __import__('12-fizzbuzz').fizzbuzz
+
+# Call the fizzbuzz function and print a new line
+fizzbuzz()
+print("")
