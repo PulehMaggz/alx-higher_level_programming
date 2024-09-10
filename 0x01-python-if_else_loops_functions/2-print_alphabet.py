528709f716fb26df6f1356825fb3d8a54335f36f
PulehMaggz <pulehmaggz@gmail.com>
2024-09-10 09:41:04 -0700

    2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
-for char in range(97, 123):  # ASCII values for 'a' to 'z'
-    print(chr(char), end='')
+for i in range(97, 123):  # ASCII values for 'a' to 'z'
+    print(chr(i), end='')
