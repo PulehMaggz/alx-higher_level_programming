1a3f1cd83858d6e7f2adf26e8c20435f3dd85eed
PulehMaggz <pulehmaggz@gmail.com>
2024-09-18 16:44:53 -0700

    Unique addition

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+    """Adds all unique integers in a list.
+
+    Args:
+        my_list (list): The initial list of integers.
+
+    Returns:
+        int: The sum of all unique integers in the list.
+    """
+    return sum(set(my_list))
