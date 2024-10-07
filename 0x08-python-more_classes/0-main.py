9c2a576003bf73717db8b57aefa25103ee628846
PulehMaggz <pulehmaggz@gmail.com>
2024-10-07 13:45:55 -0700

    Simple rectangle

--- 0x08-python-more_classes/0-main.py
+++ 0x08-python-more_classes/0-main.py
@@ -2,5 +2,5 @@
 Rectangle = __import__('0-rectangle').Rectangle
 
 my_rectangle = Rectangle()
-print(type(my_rectangle))
-print(my_rectangle.__dict__)
+print(type(my_rectangle))          # Should print the type of the object
+print(my_rectangle.__dict__)      # Should print an empty dictionary {}
