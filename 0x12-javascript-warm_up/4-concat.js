43c5560404d02965dc07f44c29775665e8f3b156
PulehMaggz <pulehmaggz@gmail.com>
2024-12-19 10:04:15 -0800

    Add script that prints two arguments in 'is' format

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+// Get the first and second arguments passed to the script
+const firstArg = process.argv[2];
+const secondArg = process.argv[3];
+
+// Print the arguments in the format " is "
+console.log(firstArg + ' is ' + secondArg);
