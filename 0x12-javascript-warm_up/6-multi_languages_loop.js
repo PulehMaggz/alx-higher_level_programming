859cd231bf35379dfc17f3def445519caafda6b7
PulehMaggz <pulehmaggz@gmail.com>
2024-12-19 10:25:26 -0800

    Add script to print multiple lines using a loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+// Create an array with the strings
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+// Use a loop to print each element in the array
+for (const language of languages) {
+  console.log(language);
+}
