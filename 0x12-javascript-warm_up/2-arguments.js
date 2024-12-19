c52e35f44eac9d5c6b3c1124d86e52a8c2978fe0
PulehMaggz <pulehmaggz@gmail.com>
2024-12-19 09:48:37 -0800

    Add script to print message based on number of arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+// Get the arguments passed to the script
+const args = process.argv;
+
+// Check the number of arguments
+if (args.length === 2) {
+  console.log('No argument');
+} else if (args.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
+
