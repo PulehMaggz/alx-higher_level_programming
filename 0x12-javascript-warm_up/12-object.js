c23b767265a6057e0ed518ab1976dbec3570144e
PulehMaggz <pulehmaggz@gmail.com>
2024-12-19 16:08:33 -0800

    Update script to replace the value of object property

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+
+// Update the value of the property
+myObject.value = 89;
+
+console.log(myObject);
