5bddc1c19036337fc2e099733b5d52fefb877e0c
Phumlani Magubane <pulehmaggz@gmail.com>
2025-02-11 13:09:53 -0800

    a script that prints the number of movies where the character “Wedge Antilles” is present.

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,39 +1,23 @@
-#!/usr/bin/env node
+#!/usr/bin/node
 
 const request = require('request');
 
-// Check if URL is provided as an argument
-if (process.argv.length < 3) {
-  console.log('Usage: ./4-starwars_count.js <API_URL>');
-  process.exit(1);
-}
+const apiUrl = process.argv[2]; // Get API URL from command line arguments
 
-// The URL of the Star Wars API passed as an argument
-const apiUrl = process.argv[2];
-
-// The character ID for Wedge Antilles
-const wedgeAntillesId = 18;
-
-// Make a request to the API URL
-request(apiUrl, (err, res, body) => {
-  if (err) {
-    console.log('Error:', err);
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
     return;
   }
 
-  // Parse the response body to JSON
-  const films = JSON.parse(body).results;
+  try {
+    const films = JSON.parse(body).results; // Parse JSON response and get the 'results' array
+    const count = films.filter(movie =>
+      movie.characters.some(character => character.includes('/18/'))
+    ).length;
 
-  // Initialize the counter for the number of films with Wedge Antilles
-  let count = 0;
-
-  // Loop through the films and check if Wedge Antilles is in the list of characters
-  films.forEach((film) => {
-    if (film.characters.some(characterUrl => characterUrl.includes(`/${wedgeAntillesId}/`))) {
-      count++;
-    }
-  });
-
-  // Print the result
-  console.log(count);
+    console.log(count);
+  } catch (parseError) {
+    console.error('Error parsing JSON:', parseError);
+  }
 });
